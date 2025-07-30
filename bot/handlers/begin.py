1cbaff7a04b9a6618f5462bf94ddfded4cbb8730
Artem <aspnmrv@Artems-MBP.IGD_Rostelecom>
2025-07-30 16:37:16 +0300

    fixed config

--- bot/handlers/begin.py
+++ bot/handlers/begin.py
@@ -15,7 +15,7 @@ async def begin(event):
     user_id = event.message.peer_id.user_id
     step = await _get_current_user_step(user_id)
 
-    if await is_expected_steps(user_id, [0]):
+    if await is_expected_steps(user_id, [0, 3]):
         await _update_current_user_step(user_id, 1)
         text = "Начнем! Спрашивай меня любые вопросы, а я постараюсь тебе помочь " \
                "с ответами 🙂\n\nКогда устанешь, нажимай на кнопку Завершить!\n\n"
